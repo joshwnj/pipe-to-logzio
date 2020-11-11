1e8940787ac8e5cc1d7c92cb96697336e764f431
Josh Johnston <josh@x-team.com>
2020-11-11 17:00:18 +1100

    support type arg

--- cli.js
+++ cli.js
@@ -2,12 +2,14 @@
 const argv = require('minimist')(process.argv.slice(2))
 
 const token = argv.t || argv.token
+const type = argv.type || 'nodejs'
 const host = argv.host || 'listener.logz.io'
 
 require('./index')(
   process.stdin, 
   {
     token,
+    type,
     host
   }
 )
