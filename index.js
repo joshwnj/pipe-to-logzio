3638a6a6c272cc49b94c203dff376ced0e4b2f4c
Josh Johnston <josh@x-team.com>
2023-08-01 10:04:42 +1000

    direct pipe to stdout

--- index.js
+++ index.js
@@ -6,16 +6,20 @@ const logzioNodejs = require('logzio-nodejs');
 module.exports = main
 
 function main (stream, options) {
-  const logger = options.token ?
-        logzioNodejs.createLogger(options) :
-        null
+  if (!options.token) {
+    console.log('[pipe-to-logzio] no token')
+    stream.pipe(process.stdout)
+    return
+  }
+
+  const logger = logzioNodejs.createLogger(options)
 
   var myTransport = through(function (chunk, enc, cb) {
     const val = chunk.toString('utf8')
     console.log(val)
-    if (logger) { logger.log(stripAnsi(val)) }
+    logger.log(stripAnsi(val))
     cb()
   })
 
-  pump(stream, myTransport)
+  pump(stream, myTransport)  
 }
